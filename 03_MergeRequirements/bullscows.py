bb8cd0a15d135701c8581c894a98199cc46ae084
sanchomerk <sanchomerk@yandex-team.ru>
2024-03-04 22:59:19 +0300

    Realise simple version of game

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,8 @@
 from collections import Counter
+from random import choice
+from urllib.request import urlopen
+import argparse
+
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = 0
@@ -12,3 +16,39 @@ def bullscows(guess: str, secret: str) -> (int, int):
         cows += min(secret_count[c], guess_count[c])
     return bulls, cows
 
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = choice(words)
+    amount_try = 0
+    while True:
+        guess = ask("Введите слово: ", words)
+        amount_try += 1
+        b, c = bullscows(guess, secret)
+        if b == len(guess):
+            print("SUCCESS WITH", amount_try, "attempts!")
+            break
+        inform("Быки: {}, Коровы: {}", b, c)
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    guess = input(prompt)
+    while valid and guess not in valid:
+        print("Word not from valid, try again")
+        guess = input(prompt)
+    return guess
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dictionary", help="sourse of words")
+parser.add_argument("length", help="length", type=int, default=5)
+args = parser.parse_args()
+possible_words = []
+if args.dictionary.split(":")[0] in ["http", "https"]:
+    with urlopen(args.dictionary) as input_url:
+        possible_words = input_url.read().decode('utf-8').splitlines()
+possible_words = list(filter(lambda w: len(w) == args.length, possible_words))
+gameplay(ask, inform, possible_words)
