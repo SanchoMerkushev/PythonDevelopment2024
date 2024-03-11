6b88561fa71dec929b7eb4a1ce69faf6d711c553
Sancho <sashamerkushev@yandex.ru>
2024-03-12 00:12:01 +0300

    Add cowthink

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -14,8 +14,7 @@ class CowSayCMd(cmd.Cmd):
             print(cowsay.list_cows(arg))
         else:
             print(cowsay.list_cows())
-    
-    
+
     def do_make_bubble(self, arg):
         '''
         Make a bubble with message
@@ -29,7 +28,6 @@ class CowSayCMd(cmd.Cmd):
         if len(arguments) > 1:
             dict_args["width"] = int(arguments[1])
         print(cowsay.make_bubble(arguments[0], **dict_args))
-
 
     def do_cowsay(self, arg):
         '''
@@ -47,6 +45,23 @@ class CowSayCMd(cmd.Cmd):
             if arguments[i] in available_options:
                 dict_args[options[arguments[i]]] = arguments[i + 1]
         print(cowsay.cowsay(arguments[0], **dict_args))
+    
+    def do_cowthink(self, arg):
+        '''
+        Cow think about a message
+        Optional arguments [-e eyes] [-t tongue] [-c cow]
+        Format of command: cowsay message [-e eyes] [-t tongue] [-c cow]
+        '''
+        dict_args = {}
+        available_options = ["-e", "-t", "-c"]
+        options = {"-e" : "eyes", "-t" : "tongue", "-c" : "cow"}
+        arguments = shlex.split(arg)
+        if not arguments:
+            return
+        for i in range(1, len(arguments) - 1):
+            if arguments[i] in available_options:
+                dict_args[options[arguments[i]]] = arguments[i + 1]
+        print(cowsay.cowthink(arguments[0], **dict_args))
         
 
 if __name__ == '__main__':
