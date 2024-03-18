73ee1156d8bef630bca20616081284f7710c149a
sanchomerk <sanchomerk@yandex-team.ru>
2024-03-19 02:12:36 +0300

    Add login and yield

--- 05_DiffPatchNet/chat.py
+++ 05_DiffPatchNet/chat.py
@@ -9,7 +9,6 @@ cows = {}
 async def chat(reader, writer):
     me = "{}:{}".format(*writer.get_extra_info('peername'))
     clients[me] = asyncio.Queue()
-    cows[me] = 'frogs'
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(clients[me].get())
     while not reader.at_eof():
@@ -23,6 +22,20 @@ async def chat(reader, writer):
                     await clients[me].put(cows.values())
                 elif command == "cows":
                     await clients[me].put((set(cowsay.list_cows()) - set(cows.values())))
+                elif command == "login":
+                    if me in cows:
+                        await clients[me].put(f"You have previous login - {cows[me]}")
+                    elif len(input_str.split()) == 1 or input_str.split()[1] not in cowsay.list_cows():
+                        await clients[me].put("Incorrect login, use cows to get list of possible names")
+                    else:
+                        cows[me] = input_str.split()[1]
+                        print(f"New user is {cows[me]}")
+                        await clients[me].put(f"You name is {cows[me]}")
+                else:
+                    if me in cows:
+                        for out in clients:
+                            if out != me and out in cows:
+                                await clients[out].put(cowsay.cowsay(q.result().decode(), cow=cows[me]))
             elif q is receive:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
